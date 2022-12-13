be977eadd4ee54386a0bf08ea39d8a7ea8be9dfe
coder-ms <marco.sbrascia@gmail.com>
2022-12-13 14:34:16 +0200

    add base partials

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// REQUIRED
+require_once __DIR__ . '/Models/User.php';
+require_once __DIR__ . '/Models/PremiumUser.php';
+require_once __DIR__ . '/Models/Membership.php';
+
+
+
+// INCLUDE
+include __DIR__ . '/partials/header.php';
+include __DIR__ . '/partials/main.php';
+include __DIR__ . '/partials/footer.php'; 
+?>
---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+<?php ?>
+<footer class="container-fluid bg-black text-white text-center">
+    <h3 class="p-4">Made with &hearts; By classe coder-ms</h3>
+</footer>
+</body>
+</html>
